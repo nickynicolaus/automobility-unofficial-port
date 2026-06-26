f53aff4072db9d27479c8ff62230ce43d4d1a362
David <davidhasnexus@gmail.com>
2026-06-26 07:50:56 +0200

    Fix grass cutter vegetation harvesting

--- common/src/main/java/io/github/foundationgames/automobility/automobile/attachment/front/GrassCutterFrontAttachment.java
+++ common/src/main/java/io/github/foundationgames/automobility/automobile/attachment/front/GrassCutterFrontAttachment.java
@@ -3,7 +3,9 @@ package io.github.foundationgames.automobility.automobile.attachment.front;
 import io.github.foundationgames.automobility.automobile.attachment.FrontAttachmentType;
 import io.github.foundationgames.automobility.entity.AutomobileEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.BushBlock;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
@@ -17,7 +19,17 @@ public class GrassCutterFrontAttachment extends BaseHarvesterFrontAttachment {
 
     @Override
     public boolean canHarvest(BlockState state) {
-        return (state.getBlock() instanceof BushBlock) && !(state.getBlock() instanceof CropBlock);
+        if (state.getBlock() instanceof CropBlock) {
+            return false;
+        }
+
+        return state.is(BlockTags.FLOWERS)
+                || state.is(Blocks.SHORT_GRASS)
+                || state.is(Blocks.TALL_GRASS)
+                || state.is(Blocks.FERN)
+                || state.is(Blocks.LARGE_FERN)
+                || state.is(Blocks.DEAD_BUSH)
+                || state.getBlock() instanceof BushBlock;
     }
 
     @Override
